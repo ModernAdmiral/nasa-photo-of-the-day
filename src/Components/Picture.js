4d11b28ff987cd646a7f02d86f24f9908631cf87
Jackson McComas <jackson.mccomas@gmail.com>
2019-10-10 17:50:33 -0500

    finished day 2 mvp

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -1,13 +1,31 @@
 // Contains picture and explination
 import React from "react";
+import styled from "styled-components";
+
+const ContentWrapper = styled.div`
+    text-align: center;
+`
+
+const ImgWrapper = styled.img`
+    border-radius: 20px;
+`
+
+const Explanation = styled.p`
+    margin-top: 2rem;
+    width: 75%;
+    margin-left: 12.5%;
+    font-family: 'Rokkitt', serif;
+    font-size: 1.5rem;
+
+`
 
 const Picture = (props) => {
 
      return(
-        <div>
-            <img alt={props.media_type} src={props.source}></img>
-            <p>{props.explanation}</p>
-        </div>
+        <ContentWrapper>
+            <ImgWrapper alt={props.media_type} src={props.source}></ImgWrapper>
+            <Explanation>{props.explanation}</Explanation>
+        </ContentWrapper>
      )
 }
 
